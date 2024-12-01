2b29b8ef208e9f6b095004dafd0654629a7c5f23
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-02 00:58:13 +0530

    third commit

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-// app.use(session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//         maxAge: 1000*60*60*24,
-//     }
-// }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000*60*60*24,
+    }
+}));
 
 
 

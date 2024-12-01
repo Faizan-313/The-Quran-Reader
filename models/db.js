d94bf6ed596d498a4384eba795acf5575baa2e0e
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-01 18:42:25 +0530

    second commit

--- models/db.js
+++ models/db.js
@@ -4,11 +4,11 @@ import env from "dotenv";
 env.config();
 
 const db = new pg.Client({
-    PGUSER: process.env.PG_USER,
-    PGHOST: process.env.PG_HOST,
-    POSTGRES_DB: process.env.PG_DATABASE,
-    POSTGRES_PASSWORD: process.env.PG_PASSWORD,
-    PGPORT: process.env.PG_PORT,
+    PGUSER: process.env.DB_USER,
+    PGHOST: process.env.DB_HOST,
+    PGDATABASE: process.env.DB_DATABASE,
+    PGPASSWORD: process.env.DB_PASSWORD,
+    PGPORT: process.env.DB_PORT,
 });
 
 export default db;

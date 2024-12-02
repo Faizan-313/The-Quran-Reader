35e6e20a5853d8ee16c35f13eedca8255c580b08
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-02 15:38:23 +0530

    17th

--- models/db.js
+++ models/db.js
@@ -3,25 +3,21 @@ import env from "dotenv";
 
 env.config();
 
-// console.log("DB User:", process.env.PG_USER);
-// console.log("DB Password:", process.env.PG_PASSWORD);
-// console.log("DB Host:", process.env.PG_HOST);
-// console.log("DB Port:", process.env.PG_PORT);
+console.log("DB User:", process.env.PG_USER);
+console.log("DB Password:", process.env.PG_PASSWORD);
+console.log("DB Host:", process.env.PG_HOST);
+console.log("DB Port:", process.env.PG_PORT);
 
-
-// const db = new pg.Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false,    
-//     },
-// });
 
 const db = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
     ssl: {
-        rejectUnauthorized: false
-    }
+        rejectUnauthorized: false,    
+    },
 });
-
 
 export default db;

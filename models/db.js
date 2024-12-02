244dfb49aed36f180ff377846528ce48ff4f1fa6
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-02 14:38:11 +0530

    13th

--- models/db.js
+++ models/db.js
@@ -4,8 +4,8 @@ import env from "dotenv";
 env.config();
 
 const db = new pg.Client({
-    user: process.env.PG_USER,
     host: process.env.PG_HOST,
+    user: process.env.PG_USER,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,

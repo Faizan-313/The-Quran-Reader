94535d0bee99046bd294dc37c0f6dbd5decfea06
Faizan-313 <faizansyedpeer@gmail.com>
2024-12-07 01:11:43 +0530

    adjustments

--- models/db.js
+++ models/db.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
     user: process.env.PG_USER,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    port: parseInt(process.env.PG_PORT),
 });
 
 export default db;
